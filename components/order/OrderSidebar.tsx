03ac7d66a54e86841d6aea46686242736ecf9499
Eduardo Hernandez <hernandezestrellaeduardo@gmail.com>
2024-06-01 14:37:09 -0600

    Nombre de las categorias con su icono

--- components/order/OrderSidebar.tsx
+++ components/order/OrderSidebar.tsx
@@ -1,4 +1,6 @@
+/* eslint-disable react/jsx-key */
 import { prisma } from "@/src/lib/prisma";
+import CategoryIcon from "../ui/CategoryIcon";
 
 async function getCategories() {
   return await prisma.category.findMany();
@@ -6,7 +8,13 @@ async function getCategories() {
 
 export default async function OrderSidebar() {
   const categories = await getCategories();
-  console.log(categories);
 
-  return <aside className="md:w-72 md:h-screen bg-white">OrderSidebar</aside>;
+  return (
+    <aside className="md:w-72 md:h-screen bg-white">
+      <nav className="mt-10"></nav>
+      {categories.map((category) => (
+        <CategoryIcon key={category.id} category={category} />
+      ))}
+    </aside>
+  );
 }
