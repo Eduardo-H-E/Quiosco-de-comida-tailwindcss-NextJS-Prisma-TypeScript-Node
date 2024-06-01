958e962752a7a3d40c6b3e7c86692a3a058c72f7
Eduardo Hernandez <hernandezestrellaeduardo@gmail.com>
2024-06-01 16:11:49 -0600

    Mostrar articulos por categoria, icono, precio y boton de agregar y resaltar categoria

--- components/ui/CategoryIcon.tsx
+++ components/ui/CategoryIcon.tsx
@@ -1,4 +1,7 @@
+"use client";
 import Image from "next/image";
+import Link from "next/link";
+import { useParams } from "next/navigation";
 import { Category } from "@prisma/client";
 
 type CategoryIconProps = {
@@ -6,14 +9,20 @@ type CategoryIconProps = {
 };
 
 export default function CategoryIcon({ category }: CategoryIconProps) {
+  const params = useParams<{ category: string }>();
+
   return (
     <div
-      className={`flex items-center gap-4 w-full border-t border-gray-200 p-3 last-of-type:border-p`}
+      className={`${
+        category.slug === params.category ? "bg-amber-400" : " "
+      } flex items-center gap-4 w-full border-t border-gray-200 p-3 last-of-type:border-p`}
     >
       <div className="w-16 h-16 relative">
         <Image fill src={`/icon_${category.slug}.svg`} alt="Imagen Categoria" />
       </div>
-      <p className="text-xl font-bold">{category.name}</p>
+      <Link className="text-xl font-bold" href={`/order/${category.slug}`}>
+        {category.name}
+      </Link>
     </div>
   );
 }
