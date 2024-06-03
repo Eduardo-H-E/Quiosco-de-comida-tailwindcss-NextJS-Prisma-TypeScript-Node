0c171bb014bda8a687c66684c91d903d07274954
Eduardo Hernandez <hernandezestrellaeduardo@gmail.com>
2024-06-02 22:43:07 -0600

    Insertar nuevo producto en conjunto de su imagen

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -1,5 +1,5 @@
 import { Product } from "@prisma/client";
-import { formatCurrency } from "../../src/lib/utils/index";
+import { formatCurrency, getImagePath } from "../../src/lib/utils/index";
 import Image from "next/image";
 import AddProductButton from "./AddProductButton";
 
@@ -8,12 +8,13 @@ type ProductCardProps = {
 };
 
 export default function ProductsCard({ product }: ProductCardProps) {
+  const imagePath = getImagePath(product.image);
   return (
     <div className="border bg-white">
       <Image
         width={400}
         height={500}
-        src={`/products/${product.image}.jpg`}
+        src={imagePath}
         alt={`Imagen platillo ${product.name}`}
       />
 
